fb7c5f5c6b5e273a9f7c7e6f14aa0196653b8c2f
Sergio Augusto <91sergioaugusto@gmail.com>
2024-10-09 18:19:10 -0400

    feat: criando um filtro de top 5 dos eps das temporadas

--- src/main/java/br/com/exemplo/screenmatch2/principal/Principal.java
+++ src/main/java/br/com/exemplo/screenmatch2/principal/Principal.java
@@ -7,8 +7,10 @@ import br.com.exemplo.screenmatch2.service.ConsumoApi;
 import br.com.exemplo.screenmatch2.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -41,5 +43,19 @@ public class Principal {
 //        }
 
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        //top 5 ep
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 episodios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
+
     }
 }
